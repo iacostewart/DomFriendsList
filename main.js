47ae6e74ffdf548f68cd5ac6cde9e94d7abce3ec
Ian Stewart <iacostewart@msn.com>
2018-01-16 16:56:44 -0500

    3rd module

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
 $(document).ready(function() {
-    console.log("ready");
+    $("#friend-form").submit( (e) => {
+        e.preventDefault();
+        let newFriend = $("#input").val();
+        let newElement = createFriend(newFriend);
+        $("#friend-list").append(newElement);
+        $("#input").val("");
+        console.log(e.currentTarget[0].value);
+    
+})
+    function creatFriend(name) {
+        return $(`<li>${name}</li>`).addClass("list-group-item list-group-item-action list-group-item-dark");
+    }
 });
+$(document).ready(function(){
+    $('#friend-form').submit( (e) => {
+        e.preventDefault();
+        let newFriend = $('#input').val();
+        let newElement = createFriend(newFriend);
+        $('#friend-list').append(newElement);
+        $('#input').val('');
+        console.log(e.currentTarget[0].value);
+    })
+    $('#friend-list').on('click', 'li', function(e) {
+        $(this).remove();
+
+    })
+    function createFriend(name) {
+        return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
+    }
+})
+// // $(document).ready(function() {
+// //     $("#friend-form").submit( (e) => {
+// //       e.preventDefault();
+// //       let newFriend = $('#input').val();
+// //       let newElement = createFriend(newFriend);
+// //       $('#friend-list').append(newElement);
+// //       $('#input').val("");
+// //       console.log(e.currentTarget[0].value);
+// //     })
+// //     $('#friend-list').on('click', 'li', function(e) {
+// //       $(this).remove();
+// //     })
+// //     function createFriend(name) {
+// //       return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
+// //     }
+// //   });

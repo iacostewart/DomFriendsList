7e1d87f95231e53742643898f3fc9ca086f0b1ab
Ian Stewart <iacostewart@msn.com>
2018-01-16 17:14:31 -0500

    Module 3 "remove Friends"

--- main.js
+++ main.js
@@ -1,47 +1,54 @@
+// $(document).ready(function() {
+//     $("#friend-form").submit( (e) => {
+//         e.preventDefault();
+//         let newFriend = $("#input").val();
+//         let newElement = createFriend(newFriend);
+//         $("#friend-list").append(newElement);
+//         $("#input").val("");
+//         console.log(e.currentTarget[0].value);
+    
+// })
+//     function creatFriend(name) {
+//         return $(`<li>${name}</li>`).addClass("list-group-item list-group-item-action list-group-item-dark");
+//     }
+// });
+// $(document).ready(function(){
+//     $('#friend-form').submit( (e) => {
+//         e.preventDefault();
+//         let newFriend = $('#input').val();
+//         let newElement = createFriend(newFriend);
+//         $('#friend-list').append(newElement);
+//         $('#input').val('');
+//         console.log(e.currentTarget[0].value);
+//     })
+//     $('#friend-list').on('click', 'li', function(e) {
+//         $(this).remove();
+
+//     })
+//     function createFriend(name) {
+//         return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
+//     }
+// })
+
+
 $(document).ready(function() {
     $("#friend-form").submit( (e) => {
-        e.preventDefault();
-        let newFriend = $("#input").val();
-        let newElement = createFriend(newFriend);
-        $("#friend-list").append(newElement);
-        $("#input").val("");
-        console.log(e.currentTarget[0].value);
-    
-})
-    function creatFriend(name) {
-        return $(`<li>${name}</li>`).addClass("list-group-item list-group-item-action list-group-item-dark");
-    }
-});
-$(document).ready(function(){
-    $('#friend-form').submit( (e) => {
-        e.preventDefault();
-        let newFriend = $('#input').val();
-        let newElement = createFriend(newFriend);
-        $('#friend-list').append(newElement);
-        $('#input').val('');
-        console.log(e.currentTarget[0].value);
+      e.preventDefault();
+      let newFriend = $('#input').val();
+      let newElement = createFriend(newFriend);
+      $('#friend-list').append(newElement);
+      $('#input').val("");
+
+      $('friend-list').on('click', 'li', function(e) {
+          console.log('clicked!');
+          console.log($(this));
+      })
+      console.log(e.currentTarget[0].value);
     })
     $('#friend-list').on('click', 'li', function(e) {
-        $(this).remove();
-
+      $(this).remove();
     })
     function createFriend(name) {
-        return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
+      return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
     }
-})
-// // $(document).ready(function() {
-// //     $("#friend-form").submit( (e) => {
-// //       e.preventDefault();
-// //       let newFriend = $('#input').val();
-// //       let newElement = createFriend(newFriend);
-// //       $('#friend-list').append(newElement);
-// //       $('#input').val("");
-// //       console.log(e.currentTarget[0].value);
-// //     })
-// //     $('#friend-list').on('click', 'li', function(e) {
-// //       $(this).remove();
-// //     })
-// //     function createFriend(name) {
-// //       return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
-// //     }
-// //   });
+  });

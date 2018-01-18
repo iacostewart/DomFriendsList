de8575526de4ae002d01e0de33c5695301838ef8
Ian Stewart <iacostewart@msn.com>
2018-01-17 22:21:34 -0500

    middle 5 friends post worked

--- main.js
+++ main.js
@@ -29,9 +29,24 @@
 //         return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
 //     }
 // })
-
+// $(() => {
 
 $(document).ready(function() {
+
+  $.ajax({
+    url: 'http://rest.learncode.academy/api/theDude/friends',
+    type: 'GET'
+  }).done( (data) => {
+    data.forEach( (eachFriend) => {
+      let name= eachFriend.firstName + " " + eachFriend.lastName;
+      let newElement = createFriend(name);
+      $('#friend-list').append(newElement);
+    })
+  }).fail( () => {
+    alert('AJAX call failed, unable to retreive data.');
+  })
+  
+
     $("#friend-form").submit( (e) => {
       e.preventDefault();
       let newFriend = $('#input').val();
@@ -52,3 +67,49 @@ $(document).ready(function() {
       return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
     }
   });
+
+
+// // example of ajax call request
+
+// /*
+// $.ajax({
+//   url: 'http://www.website.com/api',
+//   type: 'get'
+//   }).done( (data) => {
+//     //do something with the data we get back
+//   }).fail( () => {
+//     //handle the error
+//   })
+// */
+
+// // $(document).ready(function(){
+// //  })  this is the old way of doing things. the new way is bellow. 
+
+
+
+
+  $.ajax({
+   url: 'http://rest.learncode.academy/api/theDude/friends',
+  type: 'GET'
+}).done( (data) => {
+data.forEach( (eachFriend) => {
+  console.log(eachFriend.firstName + " " + eachFriend.lastName);
+})
+}).fail( () => {
+  alert('AJAX call fialed, Unable to retrive Requested DAta');
+
+})
+
+// // $.ajax({
+// //   url: 'http://rest.learncode.academy/api/theDude/friends',
+// //   type: 'GET'
+// // }).done( (data) => { 
+// //   console.log(data);
+// // })
+
+
+
+// })
+
+
+

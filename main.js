aff75107aa607eb3985718f3ed641d394ec19dae
Ian Stewart <iacostewart@msn.com>
2018-01-17 23:13:05 -0500

    Cleaned up main.js

--- main.js
+++ main.js
@@ -1,35 +1,4 @@
-// $(document).ready(function() {
-//     $("#friend-form").submit( (e) => {
-//         e.preventDefault();
-//         let newFriend = $("#input").val();
-//         let newElement = createFriend(newFriend);
-//         $("#friend-list").append(newElement);
-//         $("#input").val("");
-//         console.log(e.currentTarget[0].value);
-    
-// })
-//     function creatFriend(name) {
-//         return $(`<li>${name}</li>`).addClass("list-group-item list-group-item-action list-group-item-dark");
-//     }
-// });
-// $(document).ready(function(){
-//     $('#friend-form').submit( (e) => {
-//         e.preventDefault();
-//         let newFriend = $('#input').val();
-//         let newElement = createFriend(newFriend);
-//         $('#friend-list').append(newElement);
-//         $('#input').val('');
-//         console.log(e.currentTarget[0].value);
-//     })
-//     $('#friend-list').on('click', 'li', function(e) {
-//         $(this).remove();
 
-//     })
-//     function createFriend(name) {
-//         return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
-//     }
-// })
-// $(() => {
 
 $(document).ready(function() {
 
@@ -106,34 +75,6 @@ $.ajax({
 //     //handle the error
 //   })
 // */
-
-// // $(document).ready(function(){
-// //  })  this is the old way of doing things. the new way is bellow. 
-
-
-
-
-//   $.ajax({
-//    url: 'http://rest.learncode.academy/api/theDude/friends',
-//   type: 'GET'
-// }).done( (data) => {
-// data.forEach( (eachFriend) => {
-//   console.log(eachFriend.firstName + " " + eachFriend.lastName);
-// })
-// }).fail( () => {
-//   alert('AJAX call fialed, Unable to retrive Requested DAta');
-
-// })
-
-// // $.ajax({
-// //   url: 'http://rest.learncode.academy/api/theDude/friends',
-// //   type: 'GET'
-// // }).done( (data) => { 
-// //   console.log(data);
-// // })
-
-
-
 
 
 
